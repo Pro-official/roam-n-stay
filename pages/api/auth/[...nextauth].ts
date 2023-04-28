703b141b74de4f93553015c52b7d1e402cd01ac3
Pro-official <promise.official000@gmail.com>
2023-04-28 19:17:52 +0600

    auth providers and prisma error fix

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,13 @@
 import bcrypt from "bcrypt"
 import NextAuth, { AuthOptions } from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import CredentialsProvider from "next-auth/providers/credentials"
 
 import prisma from "@/app/libs/prismadb"
 
+// Auth options
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
@@ -44,10 +45,7 @@ export const authOptions: AuthOptions = {
           user.hashedPassword
         );
 
-        if (!isCorrectPassword) {
-          throw new Error('Invalid credentials');
-        }
-
+        if (!isCorrectPassword) throw new Error('Invalid credentials');
         return user;
       }
     })
